4edc63c93018872b0f81e639eccac9e727d52498
Nish <igor.rashyn@gmail.com>
2019-04-08 20:04:52 +1200

    Added hash/ mine and genesis function to the Block class

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
     constructor(timestamp, lastHash, hash, data){
         this.timestamp = timestamp;
@@ -9,11 +11,26 @@ class Block {
 
     toString(){
         return `Block -
-        Timestamp:${this.timestamp}
-        Last hash:${this.lastHash.substring(0,10)}
-        Hash     :${this.hash.substring(0,10)}
-        Data     :${this.data}
+        Timestamp: ${this.timestamp}
+        Last hash: ${this.lastHash.substring(0,10)}
+        Hash     : ${this.hash.substring(0,10)}
+        Data     : ${this.data}
         `;
+    }
+
+    static genesis(){
+        return new this(Date.now(), '-----', 'f1r57-h45h', []);
+    }
+
+    static mine(lastBlock, data){
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        const hash = Block.hash(timestamp, lastHash, data);
+        return new this(timestamp, lastHash, hash, data)
+    }
+
+    static hash(timestamp, lastHash, data){
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
     }
 }
 
