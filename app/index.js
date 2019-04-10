81808c94eadb31b10ad9f1a343c461b473a096e1
Nish <igor.rashyn@gmail.com>
2019-04-10 16:14:10 +1200

    added websocket p2p

--- app/index.js
+++ app/index.js
@@ -1,30 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
-const P2pServer = require('./p2p-server')
+// const P2pServer = require('./p2p-server');
+const PubSub = require('./pubsub');
 
-const HTTP_PORT = process.env.HTTP_PORT || 3001; //HTTP_PORT =3002 npm run dev
+const HTTP_PORT = process.env.HTTP_PORT || 3000; //HTTP_PORT =3002 npm run dev
 
 const app = express();
-const bc = new Blockchain();
-const p2pServer = new P2pServer(bc);
+const blockchain = new Blockchain();
+const pubsub = new PubSub({blockchain});
+// const p2pServer = new P2pServer(blockchain);
+
+setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
 
-app.get('/blocks', (req, res) => {
-    res.json(bc.chain);
+app.get('/api/blocks', (req, res) => {
+    res.json(blockchain.chain);
 });
 
-app.post('/mine', (req, res) => {
-    const block = bc.addBlock(req.body.data);
-    console.log(`New block added: ${block.toString()}`);
-    
-    res.redirect('/blocks');
+app.post('/api/mine', (req, res) => {
+    const {data} = req.body;
+    blockchain.addBlock({ data });
+
+    // p2pServer.syncChains();
+    res.redirect('/api/blocks');
 })
 
 app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}`)
 });
 
-p2pServer.listen();
+// p2pServer.listen();
 
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -2,6 +2,7 @@ const Websocket = require('ws');
 
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+// HTTP_PORT=3002 P2P_PORT=5002 PEERS=ws://localhost:5001 npm run dev
 
 class P2pServer {
     constructor(blockchain){
@@ -30,6 +31,24 @@ class P2pServer {
     connectSocket(socket){
         this.sockets.push(socket);
         console.log('Socket connected');
+        this.messageHandler(socket);
+        this.sendChain(socket);
+    }
+
+    messageHandler(socket){
+        socket.on('message', message => {
+            const data = JSON.parse(message);
+
+            this.blockchain.replace(data);
+        });
+    }
+
+    sendChain(socket){
+        socket.send(JSON.stringify(this.blockchain.chain));
+    }
+
+    syncChains(){
+        this.sockets.forEach(socket => this.sendChain(socket))
     }
 
 }
