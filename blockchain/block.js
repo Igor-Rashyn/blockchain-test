cd5842df121340b1adec3469c36594520df05f70
Nish <igor.rashyn@gmail.com>
2019-04-11 09:29:29 +1200

    restructure

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,6 +1,6 @@
 const hexToBinary = require('hex-to-binary');
 const cryptoHash = require('../util/crypto-hash');
-const { GENESIS_DATA, MINE_RATE } = require('./config');
+const { GENESIS_DATA, MINE_RATE } = require('../config');
 
 
 
