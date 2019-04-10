d58748171ca12d1c8f6a74a9b22031aead9b20ec
Nish <igor.rashyn@gmail.com>
2019-04-11 11:06:35 +1200

    crypto hash util
    - was modify to avoid geting the same hash for modified object

--- util/crypto-hash.test.js
+++ util/crypto-hash.test.js
@@ -1,7 +1,7 @@
 const cryptoHash = require('./crypto-hash');
 
 describe('cryptoHash()', () => {
-    const sha256 = 'e47fa9ad6e4766adb93578872159e81effae565d10b8c6ca8208a92622607fe9';
+    const sha256 = '7ba5e8ef7029742e81085289b8c4ffd0801355df315b573f75b533a3d31cca74';
 
     it('generates a SHA-256 hashed output',() =>{
         expect(cryptoHash('bla-bla')).toEqual(sha256);
@@ -10,4 +10,12 @@ describe('cryptoHash()', () => {
     it('produces the same hash with the same input arguments in any order',() =>{
         expect(cryptoHash('one', 'two', 'three')).toEqual(cryptoHash('two', 'three', 'one'));
     })
+
+    it('produces a unique hash when the properties have changed on an input', () => {
+        const boom = {};
+        const originalHash = cryptoHash(boom);
+        boom['field'] = 'abc';
+
+        expect(cryptoHash(boom)).not.toEqual(originalHash);
+    });
 });
